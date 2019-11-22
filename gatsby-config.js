47d20b49e7cf47fed4265f0809ed37bad3e89326
fydy <39726855+fydy@users.noreply.github.com>
2019-11-22 13:22:53 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,3 +32,39 @@ module.exports = {
     // `gatsby-plugin-offline`,
   ],
 }
+
+module.exports = {
+    plugins: [{
+        resolve: "gatsby-theme-blog-with-github",
+        options: {
+            // your github username - required
+            username: "fydy",
+            
+			// github public repository name that you will use as a CMS - required
+            repositoryName: "issue-blog",
+            
+            metaData: {
+                // website name - required
+                title: `E-Law`,
+                
+				// cover letter - required
+                description: `分享`,
+
+				// author name
+                author: `fydy`, 
+
+				// your github url for photo - required
+                githubURL: `https://github.com/fydy`, 
+                social: [{
+                        name: `twitter`,
+                        url: `https://twitter.com/chunfengxia`,
+                    },
+                    {
+                        name: `github`,
+                        url: `https://github.com/fydy`,
+                    },
+                ]
+            }
+        }
+    }]
+};
